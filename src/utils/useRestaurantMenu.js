2ae23c7e1991adc2f1ae11cf008d23c394934216
Harsh-Sindhav <harsh.sindhav@gmail.com>
2023-09-10 18:37:54 +0530

    episode-10

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -2,27 +2,17 @@ import { useEffect, useState } from "react";
 import { MENU_API } from "./constants";
 
 const useRestaurantMenu = (resId) => {
-    const [resInfo,setResInfo] = useState(null);
+  const [resInfo, setResInfo] = useState(null);
 
   useEffect(() => {
     fetchData();
   }, []);
-  const fetchData = async () =>{
+  const fetchData = async () => {
     const data = await fetch(MENU_API + resId);
     const json = await data.json();
     setResInfo(json?.data);
-  }
+  };
   return resInfo;
 };
 
 export default useRestaurantMenu;
-
-// useEffect(() => {
-//     fetchMenu();
-//   }, []);
-
-//   const fetchMenu = async () => {
-//     const data = await fetch(MENU_API + resId);
-//     const json = await data.json();
-//     setResInfo(json?.data);
-//   };
